bccc36b7a2a23f1636508da631e7a88b86372097
Alex Segers <a.segers.dev@gmail.com>
2018-09-26 05:26:02 +0000

    Done.

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -4,8 +4,8 @@ import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews';
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const BASE_URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-                 + `api-key=${NYT_API_KEY}&query=`;
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
+                 + `api-key=${NYT_API_KEY}`;
 
 class SearchableMovieReviewsContainer extends Component {
 
@@ -22,8 +22,10 @@ class SearchableMovieReviewsContainer extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
+    const searchTerm = this.state.searchTerm
+    const S_URL = URL.concat(`&query=${searchTerm}`)
 
-    fetch(BASE_URL.concat(this.state.searchTerm))
+    fetch(S_URL)
       .then(resp => resp.json())
       .then(resp => this.setState({ reviews: resp.results }));
   }
