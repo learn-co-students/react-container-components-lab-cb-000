ed49693258d0269df2fb1549721ab553e56b4ce4
derrick b <dbozich1@gmail.com>
2018-10-27 20:00:00 +0000

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,37 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      reviews: []
+    }
+  }
+
+  getReviews = () => {
+    fetch(URL).then(res => res.json()).then(reviews => {this.makeReviews(reviews.results)})
+  }
+
+  makeReviews = (reviews) => {
+    let results = reviews.map((review, index) => <li><MovieReviews review={review} key={index} /></li>)
+    this.setState({reviews: results})
+  }
+
+
+
+  render(){
+    this.getReviews
+    return(
+      <div className="latest-movie-reviews">
+        <button onClick={this.getReviews}>Latest Reviews</button>
+        <ul className="review-list">
+          {this.state.reviews.length > 0 ? this.state.reviews : '' }
+        </ul>
+      </div>
+
+    )
+  }
+}
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,12 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = ({review, key}) => {
+  return(
+    <div className="review" data-key={key}>
+      <a href={review.link.url} target="_blank"  >{review.display_title} </a>
+    </div>
+  )
+}
+
+export default MovieReviews
